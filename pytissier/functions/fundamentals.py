35d0cab86953ba952864852d5fc648f5a14aa1e9
arditobryan <ardito.bryan@gmail.com>
2024-07-08 07:53:35 +0200

    parameterized shortcrust pastry values and added whipped dough option

--- pytissier/functions/fundamentals.py
+++ pytissier/functions/fundamentals.py
@@ -1,138 +1,91 @@
 class main_fn():
 
-    def shortcrust_pastry(self, butter_percentage, sugar_percentage, sugar_type, liquid_type):
+    def shortcrust_pastry(self, butter_percentage, sugar_percentage, dough_type, sugar_type, liquid_type):
         """
         sources: https://www.fontelatavola.it/magiadeisaporiok/2016/02/09/la-pasta-frolla/
         """
 
-        valid_sugar_types = ['icing_sugar', 'granulated_sugar']
+        valid_dough_type = ['regular', 'whipped']
+        if dough_type not in valid_dough_type:
+            raise Exception("dough_type can eithe be: '"+ "', '".join(valid_dough_type) + "'")
+        
+        valid_sugar_types = ['icing', 'granulated']
         if sugar_type not in valid_sugar_types:
             raise Exception("sugar_type can eithe be: '"+ "', '".join(valid_sugar_types) + "'")
         
         valid_liquid_types = ['yolks', 'eggwhites', 'full_egg', 'water', 'milk', 'cream']
         if liquid_type not in valid_liquid_types:
-            raise Exception("sugar_type can eithe be: '"+ "', '".join(valid_liquid_types) + "'")
+            raise Exception("liquid_type can eithe be: '"+ "', '".join(valid_liquid_types) + "'")
         
         flower = 1000
         butter = flower*butter_percentage
         sugar = flower*sugar_percentage
         base = flower + butter + sugar
+        liquids = -1
 
-        match sugar_type:
-            case 'icing_sugar':
-        
-                match liquid_type:
-                    case 'yolks':
-                        match butter:
-                            case 400:
-                                liquids = base/10 + 40
-                            case 500:
-                                liquids = base/10
-                            case 600:
-                                liquids = base/10 - 40
+        match dough_type:
+            case 'regular':
 
-                    case 'eggwhites':
-                        match butter:
-                            case 400:
-                                liquids = base/23 + 35
-                            case 500:
-                                liquids = base/23
-                            case 600:
-                                liquids = base/23 - 35
+                match sugar_type:
+                    case 'icing':
 
-                    case 'full_egg':
-                        match butter:
-                            case 400:
-                                liquids = base/20 + 35
-                            case 500:
-                                liquids = base/20
-                            case 600:
-                                liquids = base/20 - 35
+                        match liquid_type:
+                            case 'yolks':          # base/10 + ((butter-500)/100)*-40
+                                liquids = base/10 + ((butter-500)/100)*-40
+                                
+                            case 'eggwhites':
+                                liquids = base/23 + ((butter-500)/100)*-35
 
-                    case 'water':
-                        match butter:
-                            case 400:
-                                liquids = base/21 + 40
-                            case 500:
-                                liquids = base/21
-                            case 600:
-                                liquids = base/21 - 40
+                            case 'full_egg':
+                                liquids = base/20 + ((butter-500)/100)*-35
 
-                    case 'milk':
-                        match butter:
-                            case 400:
-                                liquids = base/16.5 + 50
-                            case 500:
-                                liquids = base/16.5
-                            case 600:
-                                liquids = base/16.5 - 50
+                            case 'water':
+                                liquids = base/21 + ((butter-500)/100)*-40
 
-                    case 'cream':
-                        match butter:
-                            case 400:
-                                liquids = base/14.5 + 54
-                            case 500:
-                                liquids = base/14.5
-                            case 600:
-                                liquids = base/14.5 - 54
+                            case 'milk':
+                                liquids = base/16.5 + ((butter-500)/100)*-50
 
-            case 'granulated_sugar':
-        
-                match liquid_type:
-                    case 'yolks':
-                        match butter:
-                            case 400:
-                                liquids = base/6 + 75
-                            case 500:
-                                liquids = base/6
-                            case 600:
-                                liquids = base/6 - 75
+                            case 'cream':
+                                liquids = base/14.5 + ((butter-500)/100)*-54
 
-                    case 'eggwhites':
-                        match butter:
-                            case 400:
-                                liquids = base/14 + 75
-                            case 500:
-                                liquids = base/14
-                            case 600:
-                                liquids = base/14 - 75
+                    case 'granulated':
+                        match liquid_type:
+                            case 'yolks':
+                                liquids = base/6 + ((butter-500)/100)*-75
 
-                    case 'full_egg':
-                        match butter:
-                            case 400:
-                                liquids = base/12 + 60
-                            case 500:
-                                liquids = base/12
-                            case 600:
-                                liquids = base/12 - 60
+                            case 'eggwhites':
+                                liquids = base/14 + ((butter-500)/100)*-75
 
-                    case 'water':
-                        match butter:
-                            case 400:
-                                liquids = base/16 + 62
-                            case 500:
-                                liquids = base/16
-                            case 600:
-                                liquids = base/16 - 62
+                            case 'full_egg':
+                                liquids = base/12 + ((butter-500)/100)*-60
 
-                    case 'milk':
-                        match butter:
-                            case 400:
-                                liquids = base/12.5 + 80
-                            case 500:
-                                liquids = base/12.5
-                            case 600:
-                                liquids = base/12.5 - 80
+                            case 'water':
+                                liquids = base/16 + ((butter-500)/100)*-62
 
-                    case 'cream':
-                        match butter:
-                            case 400:
-                                liquids = base/11 + 65
-                            case 500:
-                                liquids = base/11
-                            case 600:
-                                liquids = base/11 - 65
+                            case 'milk':
+                                liquids = base/12.5 + ((butter-500)/100)*-80
 
+                            case 'cream':
+                                liquids = base/11 + ((butter-500)/100)*-65
+                         
+            case 'whipped':
+            
+                match sugar_type:
+                    case 'icing':
+                        
+                        match liquid_type:
+                            case 'yolks':
+                                liquids = base/10 + ((butter-500)/100)*-40
+
+                            case 'full_egg':
+                                liquids = base/20 + ((butter-500)/100)*-35
+
+                            case _:
+                                raise Exception("whipped dough only supports liquid_type: 'yolks' or 'full_egg'")
+                                
+                    case 'granulated':
+                        raise Exception("whipped dough does not support granulated sugar")
+                
         return {
             'flower' : flower,
             'butter' : butter,
